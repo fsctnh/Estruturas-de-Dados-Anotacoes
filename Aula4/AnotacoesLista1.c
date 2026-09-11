8166712d75e8edb0190fb9d3b473938cffa92e5b
Felipe Coutinho <154927793+fscoutinhoo@users.noreply.github.com>
2026-09-11 18:50:34 -0300

    exemplo adicionado, skip this

--- Aula4/AnotacoesLista1.c
+++ Aula4/AnotacoesLista1.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main(){
+    //Conceitos:
+    //Fila: FIFO (First in, First Out)
+    //Pilha: LIFO (Last In, First Out)
+
+    struct aluno{
+        int matricula;
+        char nome[30];
+        float n1, n2, n3;
+    };
+
+    struct lista{
+        int quant;
+        struct aluno dados[100]
+    };
+
+    //LISTA ESTÁTICA:
+    Lista *li;
+    li = (Lista*)malloc(sizeof(struct lista));
+    if (li != NULL){
+        li->quant =0; // ponteiro numa estrutura, usar a seta é mais legível e consistente, equivalente ao ponto
+    }
+    return li;
+
+
+
+    return 0;
+}
